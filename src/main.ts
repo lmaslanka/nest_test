1894103f914838582a73a46f437b518b3d21bffe
Lukasz Maslanka <lukasz.maslanka@gmail.com>
2023-07-17 21:35:52 -0400

    git actions

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import * as fs from 'fs';
+import * as Yaml from 'json-to-pretty-yaml';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,6 +15,10 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
+
+  const data = JSON.stringify(document);
+
+  fs.writeFileSync('./docs/swagger-spec.yaml', Yaml.stringify(data));
 
   await app.listen(3000);
 }
